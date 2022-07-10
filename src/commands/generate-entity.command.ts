2124482e6b7d3b6abc955688ae79a175c15552a2
Thierry Pelais de Oliveira <compasso-thierry.oliveira@ailos.coop.br>
2022-07-10 11:12:25 -0300

    feat: add generate entity command

---
+++ src/commands/generate-entity.command.ts
@@ -0,0 +1,44 @@
+import { InputBoxOptions, Uri, window } from "vscode";
+import * as fs from 'fs';
+import { toSnakeCase } from "../extension";
+
+export const generateEntity = async (uri: Uri) => {
+window.showInformationMessage("Generate entity");
+    const entityName = await promptForEntityName();
+    if (!entityName || entityName.length === 0) {
+        window.showErrorMessage('Please, provide a valid entity name');
+        return;
+    }
+    const entitiesPath = `${uri.fsPath}/entities`;
+    const entityPath = `${entitiesPath}/${toSnakeCase(entityName)}.dart`;
+
+    if (fs.existsSync(entitiesPath)) {
+        if (fs.existsSync(entityPath)) {
+            window.showErrorMessage(`Entity ${entityName} already exists`);
+        } else {
+            createEntityFile(entityName, entityPath);
+        }
+    } else {
+        fs.mkdirSync(entitiesPath, { recursive: true });
+        createEntityFile(entityName, entityPath);
+    }
+};
+
+
+function createEntityFile(entityName: string, entityPath: string) {
+    const content = `class ${entityName} {
+        //TODO: implement your entity properties
+      
+        //TODO: implement equality
+      }
+      `;
+    fs.writeFileSync(entityPath, content);
+    window.showInformationMessage(`Entity ${entityName} created`);
+}
+function promptForEntityName(): Thenable<string | undefined> {
+    const options: InputBoxOptions = {
+      prompt: "Entity name",
+      placeHolder: "UserEntity",
+    };
+    return window.showInputBox(options);
+  }
--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { generateEntity } from './commands/generate-entity.command';
 import { generateUsecase } from './commands/generate-usecase.command';
 
 
@@ -8,7 +9,8 @@ import { generateUsecase } from './commands/generate-usecase.command';
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 	context.subscriptions.push(
-		vscode.commands.registerCommand('flutter-clean-arch.generateUsecase', generateUsecase)
+		vscode.commands.registerCommand('flutter-clean-arch.generateUsecase', generateUsecase),
+		vscode.commands.registerCommand('flutter-clean-arch.generateEntity', generateEntity)
 	);
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
