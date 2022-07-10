c958966c8a27a39c4afb151d8bb874078e4d6263
Thierry Pelais de Oliveira <compasso-thierry.oliveira@ailos.coop.br>
2022-07-10 17:30:57 -0300

    fix: change conditions to create folder or file on domain layer

--- src/commands/domain-layer/generate-usecase.command.ts
+++ src/commands/domain-layer/generate-usecase.command.ts
@@ -11,31 +11,71 @@ window.showInformationMessage("Generate usecase");
     }
     // check if usecases directory exists
     const usecasesPath = `${uri.fsPath}/usecases`;
-    const usecasePath = `${usecasesPath}/${toSnakeCase(usecaseName)}.dart`;
+    const usecaseInterfacePath = `${usecasesPath}/interfaces/${toSnakeCase(usecaseName)}.dart`;
+    const usecaseImplementationPath = `${usecasesPath}/implementations/${toSnakeCase(usecaseName)}_impl.dart`;
+    const interfacePath = usecaseInterfacePath.substring(0, usecaseInterfacePath.lastIndexOf('/'));
+    const implementationPath = usecaseImplementationPath.substring(0, usecaseImplementationPath.lastIndexOf('/'));
+
 
     if (fs.existsSync(usecasesPath)) {
-        // check if usecase file exists
-        if (fs.existsSync(usecasePath)) {
+        if (fs.existsSync(interfacePath)) {
+            if (fs.existsSync(usecaseInterfacePath)) {
             window.showErrorMessage(`Usecase ${usecaseName} already exists`);
+            } else {
+                createUsecaseInterfaceFile(usecaseName, usecaseInterfacePath);
+            }
         } else {
-            createUsecaseInterfaceFile(usecaseName, usecasePath);
+            fs.mkdirSync(interfacePath, { recursive: true });
+            createUsecaseInterfaceFile(usecaseName, usecaseInterfacePath);
         }
+
+        if (fs.existsSync(implementationPath)) {
+            if (fs.existsSync(usecaseImplementationPath)) {
+            window.showErrorMessage(`Usecase ${usecaseName} already exists`);
+            } else {
+                createUsecaseImplementationFile(usecaseName, usecaseImplementationPath);
+            }
+        }
+        else {
+            fs.mkdirSync(implementationPath, { recursive: true });
+            createUsecaseImplementationFile(usecaseName, usecaseImplementationPath);
+        }
+        
     } else {
-        fs.mkdirSync(usecasesPath, { recursive: true });
-        createUsecaseInterfaceFile(usecaseName, usecasePath);
+        fs.mkdirSync(interfacePath, { recursive: true });
+        createUsecaseInterfaceFile(usecaseName, usecaseInterfacePath);
+        fs.mkdirSync(implementationPath, { recursive: true });
+        createUsecaseImplementationFile(usecaseName, usecaseImplementationPath);
+
     }
 };
 
 
-function createUsecaseInterfaceFile(usecaseName: string, usecasePath: string) {
+function createUsecaseInterfaceFile(usecaseName: string, path: string) {
     const content = `abstract class ${usecaseName} {
         //TODO: Fix entity name
         Future<Failure, ExampleEntity> call();
     }
       `;
+    fs.writeFileSync(path, content);
+    window.showInformationMessage(`Usecase ${usecaseName} created`);
+}
+
+function createUsecaseImplementationFile(usecaseName: string, usecasePath: string) {
+    const content = `
+    import '../interfaces/${toSnakeCase(usecaseName)}.dart';
+
+    class ${usecaseName}Impl implements ${usecaseName} {
+        @override
+        Future<Failure, ExampleEntity> call() async {
+
+        }
+    }
+    `;
     fs.writeFileSync(usecasePath, content);
     window.showInformationMessage(`Usecase ${usecaseName} created`);
 }
+
 function promptForUsecaseName(): Thenable<string | undefined> {
     const usecaseNamePromptOptions: InputBoxOptions = {
       prompt: "Usecase name",
